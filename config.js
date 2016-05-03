fbcbb308d8afd2c3b69c0a741395b5272107bf5a
Rachel Chen <me@rachelchen.me>
2016-05-02 23:34:04 -0700

    version bump

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ var self = module.exports = {
 			delay: 2000
 		}
 	},
+	debug: require('./config.json').debug,
 	remoteSecret: require('./config.json').remoteSecret,
 	s3: require('./config.json').s3,
 	apiEndpoint: function() {
