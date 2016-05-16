c5cbbe68119fbcffe3ca51e375df863f84e8f0bf
Rachel Chen <me@rachelchen.me>
2016-05-15 22:48:27 -0700

    possible EventEmitter memory leak detected

--- GC.js
+++ GC.js
@@ -6,11 +6,12 @@ var messageQ = new Queue('dermail-mta', config.redisQ.port, config.redisQ.host);
 var minutes = config.cleanInterval,
 	the_interval = minutes * 60 * 1000;
 
+messageQ.on('cleaned', function (job, type) {
+	console.log('Cleaned %s %s jobs on', job.length, type);
+});
+
 setInterval(function() {
 	messageQ.clean(5000);
-	messageQ.on('cleaned', function (job, type) {
-		console.log('Cleaned %s %s jobs on', job.length, type);
-	});
 }, the_interval)
 
 console.log('Process ' + process.pid + ' is running to clean up garbage in the queue every ' + minutes + ' minutes.')
